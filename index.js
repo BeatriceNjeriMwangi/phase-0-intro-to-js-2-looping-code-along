2e7731d51a8df66e7c688bbf57252e2a9a1eb961
beatricenjerimwangi <bbeatricemwangi@gmail.com>
2023-10-13 09:54:54 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names=["Charlie", "Samip", "Ali"];
+const event=["suprise"]
+function writeCards(names,event){
+    const message =[]
+    for(const name of names){
+        message.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+    }
+    return message
+
+}
+ 
+function countDown(number){
+    while(number>=0){
+        console.log(number);
+        number--;
+    }
+}
